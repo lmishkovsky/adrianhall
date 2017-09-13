4c143e1725c34b2cbb4b321745bf6c495a051be2
lmishkovsky <brightsoftbg@outlook.com>
2017-09-13 18:22:30 +0300

    * DroidLoginProvider.cs: Client flow for iOS and Facebook. Server flow for Android.
    * Info.plist:
    * packages.config:
    * TaskList.iOS.csproj:
    * iOSLoginProvider.cs:

--- Droid/Services/DroidLoginProvider.cs
+++ Droid/Services/DroidLoginProvider.cs
@@ -18,6 +18,16 @@ namespace TaskList.Droid.Services
 
 		public async Task LoginAsync(MobileServiceClient client)
 		{
+            // ANDROID SERVER FLOW AUTHENTICATION
+
+			// this method is different from Adrian's book where they use version 3.0 of the client
+			// Version 4.0.1 did not work for me either
+			// this working version is 4.0.0
+			// the method below accepts 3 parameters 
+			// the 3rd parameter is specified in azure
+			// ALLOWED EXTERNAL REDIRECT URLS: appname://easyauth.callback
+			// We also need to specify this parameter in the AndroidManifest.xml file:
+			// <data android:scheme="appname" android:host="easyauth.callback" /> 
 			await client.LoginAsync(context, "facebook", "appname");
 		}
 	}   
--- iOS/Services/iOSLoginProvider.cs
+++ iOS/Services/iOSLoginProvider.cs
@@ -1,19 +1,62 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Facebook.LoginKit;
+using Foundation;
+//using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json.Linq;
 using TaskList.Abstractions;
+using TaskList.Helpers;
 using TaskList.iOS.Services;
 using UIKit;
+using Xamarin.Auth;
 
 [assembly: Xamarin.Forms.Dependency(typeof(iOSLoginProvider))]
 namespace TaskList.iOS.Services
 {
 	public class iOSLoginProvider : ILoginProvider
 	{
-		public async Task LoginAsync(MobileServiceClient client)
+        public UIViewController RootView => UIApplication.SharedApplication.KeyWindow.RootViewController;
+		
+        /// <summary>
+        /// Logins the async.
+        /// </summary>
+        /// <returns>The async.</returns>
+        /// <param name="client">Client.</param>
+		// public async Task<MobileServiceUser> LoginAsync(MobileServiceClient client)
+        public async Task LoginAsync(MobileServiceClient client)
 		{
-			await client.LoginAsync(RootView, "facebook", "");
+			var accessToken = await LoginFacebookAsync();
+			var zumoPayload = new JObject();
+			zumoPayload["access_token"] = accessToken;
+            MobileServiceUser user = await client.LoginAsync("facebook", zumoPayload);		
+		}		
+
+		#region Facebook Client Flow
+		private TaskCompletionSource<string> fbtcs;
+
+		public async Task<string> LoginFacebookAsync()
+		{
+			fbtcs = new TaskCompletionSource<string>();
+			var loginManager = new LoginManager();
+
+			loginManager.LogInWithReadPermissions(new[] { "public_profile" }, RootView, LoginTokenHandler);
+			return await fbtcs.Task;
 		}
 
-		public UIViewController RootView => UIApplication.SharedApplication.KeyWindow.RootViewController;
+		private void LoginTokenHandler(LoginManagerLoginResult loginResult, NSError error)
+		{
+			if (loginResult.Token != null)
+			{
+				fbtcs.TrySetResult(loginResult.Token.TokenString);
+			}
+			else
+			{
+				fbtcs.TrySetException(new Exception("Facebook Client Flow Login Failed"));
+			}
+		}
+		#endregion
 	}
 }
